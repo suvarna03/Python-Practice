ee70be04b9153432b2776477394aaa3d32e95500
suvarna03 <75775060+suvarna03@users.noreply.github.com>
2023-01-09 13:11:52 +0530

    Create find_Nth_largest_number.py

---
+++ List/find_Nth_largest_number.py
@@ -0,0 +1,11 @@
+##### Python program to find N largest number in given list using sorting
+# list =[23,56,76,100,45,32,70,98,8,6,9,2]
+# for i in range(len(list)):
+#     for j in range(i+1, len(list)):
+#         if list[i] < list [j]:
+#             temp = list[i]
+#             list[i] = list[j]
+#             list[j] = temp
+# print(list)
+# n = int(input("Enter the largest number"))
+#print(list[n)

2a72c397c5780dc19cc17abf9486610e2dd93172
suvarna03 <75775060+suvarna03@users.noreply.github.com>
2023-01-09 15:19:55 +0530

    Create Interchange_firstElement_to_lastElement.py

---
+++ List/Interchange_firstElement_to_lastElement.py
@@ -0,0 +1,8 @@
+# list = [1,2,3,4,5,6,7,8,9]
+# print("******* Before Swapping the list ***********")
+# print(list)
+# temp = list[0]
+# list[0] = list[len(list)-1]
+# list[len(list)-1] = temp
+# print("******* After Swapping the list ***********")
+# print(list)

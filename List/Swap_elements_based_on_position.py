91de86c1eba432d38636d12c1bfe62302c02238d
suvarna03 <75775060+suvarna03@users.noreply.github.com>
2023-01-09 15:18:08 +0530

    Create Swap_elements_based_on_position.py

---
+++ List/Swap_elements_based_on_position.py
@@ -0,0 +1,10 @@
+#### Python program to swap two elements in a list based on the position
+# list =[23,56,76,100,45,32,70,98]
+# pos1 = int(input("Enter the 1st element you want to swap"))
+# pos2 = int(input("Enter the 2nd element you want to swap"))
+# last = pos2-1
+# first = pos1-1
+# temp = list[first]
+# list[first] = list[last]
+# list[last] = temp
+# print(list)

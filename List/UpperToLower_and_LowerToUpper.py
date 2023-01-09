7af12bfa6464c23cf6e31aa3a58d5d627eeb0730
suvarna03 <75775060+suvarna03@users.noreply.github.com>
2023-01-09 15:26:11 +0530

    Create UpperToLower_and_LowerToUpper.py

---
+++ List/UpperToLower_and_LowerToUpper.py
@@ -0,0 +1,17 @@
+###convert upper case to lower and vice versa
+# # string = input("Enter the string")
+# # converted_str = ''
+# string = "!@#$"
+# for i in range (len(string)):
+#     if string[i].isdecimal():
+#         pass
+#
+#     elif((string[i] >= "A" and string[i] <= 'Z') or (string[i] >= "a" and string[i] <= 'z')):
+#         if (ord(string[i]) >= 65 and ord(string[i]) <= 90):
+#             print(chr(ord(string[i])+ 32))
+#         else:
+#             print(chr(ord(string[i]) - 32))
+#     else:
+#         print("Please enter the valid string")
+#         break
+#

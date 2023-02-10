85010f0aa538891e76ffd77715998a386c65e83e
suvarna03 <75775060+suvarna03@users.noreply.github.com>
2023-02-10 13:07:48 +0530

    Create Practice.py

---
+++ String/Practice.py
@@ -0,0 +1,27 @@
+####Python – Replace characters  in String list
+## here we are replacing 'P/p' to "N/n" and 's/S' to 'q/Q'
+
+# str = 'Pythonp SLearnings'
+# li = list(str.replace(" ",""))
+# string ='PpSs'
+# print("********Before replacing the string********")
+# print(li)
+# for i in range(len(li)):
+#     if li[i] in string:
+#         li[i] = chr(ord(li[i])-2)
+# print("********After replacing the string********")
+# print(li)
+#
+
+
+### Count number of unique elements
+# elements = [1, 2, 9, 18, 89,9, 9, 9, 2, 5, 8, 7, 4, 4, 8, 1]
+# empty = [] ## holds unique elements
+# count = 0 ## count number unique elements
+# for i in elements:
+#     if i not in empty:
+#         empty.append(i)
+#         count += 1
+#
+# print(empty)
+# print("Unique elements are : ", count)
